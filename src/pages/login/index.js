78e8002afd02d2ac29b41112512bdc70fd42100e
Shriya Patel <131158049+siyaptl@users.noreply.github.com>
2025-03-20 17:09:21 +0530

    Image added to login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css"; // Ensure this CSS file is linked
+import aboutimg from '../../assets/aboutimg.jpg';
 
 const Login = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -17,38 +18,43 @@ return (
         <div className={`login-text ${isExpanded ? "expand" : ""}`}>  
               {/*  on expand  */}
             <div className={`${isExpanded ? "block" : "hidden"}`}>
+
               {/* Transition container for smooth effect */}
             <div className={`transition-container`}>
-            <div className={`form-wrapper ${isloginpage ? "is-login" : "is-signup"}`}>
-            {/* login form */}  
-              { isloginpage ? (
-                // login
-                <div className={`form-box ${isloginpage ? "" : "hidden"}`}>
-                <div className=" ml-[5%] w-[51%] mt-[41px] p-5">
-                <p className="text-white text-[41px]">Login</p>
-                <hr className="w-[71px]  mb-5 mt-[3px]"></hr>
-                <input className="usernamebox" type="text" id="userid" placeholder="Username" />
-                <br></br>
-                <input className="usernamebox passwordbox" type="password" id="pwid" placeholder="Password" />
-                <br />
-                <button className="login-btn">Log In</button>
-                <button className="signup-btn" onClick={() => {setIsLoginPage(false); user.value = ""; password.value=""}}>Sign Up</button> 
-              </div>
-              </div>
-              ):(
-                // signup
-                <div className=" ml-[71%] w-[51%] mt-[41px] p-5">
-                <p className="text-white text-[41px]">SignUp</p>
-                <hr className="w-[71px]  mb-5 mt-[3px]"></hr>
-                <input className="usernamebox" type="text"  id="userid" placeholder="Username" />
-                <br></br>
-                <input className="usernamebox" type="password"  id="pwid" placeholder="Password" />
-                <br />
-                <button className="login-btn"  onClick={() => {setIsLoginPage(true);user.value = ""; password.value=""}}>Log In</button>
-              <button className="signup-btn">Sign Up</button> 
-              </div>
-              )}
-              </div>
+              <div className={`form-wrapper ${isloginpage ? "is-login" : "is-signup"}`}>
+              {/* login form */}  
+                { isloginpage ? (
+                  // login
+                  <div className={`form-box flex flex-row border border-red-100 ${isloginpage ? "" : "hidden"}`}>
+                    <div className=" ml-[5%] mt-[41px] p-5 pr-0 flex-row">
+                    <p className="text-white text-[41px]">Login</p>
+                    <hr className="w-[71px]  mb-5 mt-[3px]"></hr>
+                    <input className="usernamebox" type="text" id="userid" placeholder="Username" />
+                    <br></br>
+                    <input className="usernamebox passwordbox" type="password" id="pwid" placeholder="Password" />
+                    <br />
+                    <button className="login-btn">Log In</button>
+                    <button className="signup-btn" onClick={() => {setIsLoginPage(false); user.value = ""; password.value=""}}>Sign Up</button> 
+                    </div>
+                    {/* img */}
+                    <div className="ml-[3px] mt-[41px] p-5 pl-0">
+                      <img src={aboutimg} className="w-[431px] h-full object-cover" alt="LOGIN" />
+                    </div>
+                </div>
+                ):(
+                  // signup
+                  <div className=" ml-[71%] w-[51%] mt-[41px] p-5">
+                  <p className="text-white text-[41px]">SignUp</p>
+                  <hr className="w-[71px] mb-5 mt-[3px]"></hr>
+                  <input className="usernamebox" type="text"  id="userid" placeholder="Username" />
+                  <br></br>
+                  <input className="usernamebox" type="password"  id="pwid" placeholder="Password" />
+                  <br />
+                  <button className="login-btn"  onClick={() => {setIsLoginPage(true);user.value = ""; password.value=""}}>Log In</button>
+                <button className="signup-btn">Sign Up</button> 
+                </div>
+                )}
+                </div>
               </div>
             </div>
 
